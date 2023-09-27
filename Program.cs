648436a581a4347fd377630429feac40767fe750
Cirino Laganà <91277970+ciruzzososa@users.noreply.github.com>
2023-09-27 10:40:19 +0200

    Esercizio 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+#region Vars
+
+List<string> contacts = (await File.ReadAllLinesAsync("rubrica.json"))
+    .ToList();
+
+#endregion
+
+#region Methods
+
+void PrintContact(string contact)
+{
+    ContactModel contactModel = JsonSerializer.Deserialize<ContactModel>(contact);
+    
+    Console.WriteLine($"{contactModel.Nome} {contactModel.Cognome}: {contactModel.Numero}");
+}
+
+#endregion
+
+#region Esercizio 1
+
+string firstContact = contacts.First();
+
+PrintContact(firstContact);
+
+#endregion
+
+#region Models
+
+class ContactModel
+{
+    public string Nome { get; set; }
+    public string Cognome { get; set; }
+    public string Numero { get; set; }
+}
+
+#endregion
